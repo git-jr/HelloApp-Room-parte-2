39b04b7dab6b52bb2873fd0f8b2f090e94241fe8
Junior <35709152+git-jr@users.noreply.github.com>
2023-02-13 16:34:55 -0300

    aual 4.3

--- app/src/main/java/br/com/alura/helloapp/di/module/DatabaseModule.kt
+++ app/src/main/java/br/com/alura/helloapp/di/module/DatabaseModule.kt
@@ -16,11 +16,19 @@ import javax.inject.Singleton
 
 private const val DATABASE_NAME = "helloApp.db"
 
-val MIGRATION_1_2 = object : Migration(1,2){
+val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("CREATE TABLE IF NOT EXISTS Usuario (`nomeDeUsuario` TEXT NOT NULL, `senha` TEXT NOT NULL, PRIMARY KEY(`nomeDeUsuario`))")
     }
+}
 
+val MIGRATION_5_6 = object : Migration(5, 6) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("CREATE TABLE IF NOT EXISTS ContatoCopia (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `nome` TEXT NOT NULL, `sobrenome` TEXT NOT NULL, `telefone` TEXT NOT NULL, `fotoPerfil` TEXT NOT NULL, `aniversario` INTEGER, `idUsuario` TEXT NOT NULL DEFAULT '', FOREIGN KEY(`idUsuario`) REFERENCES `Usuario`(`idUsuario`) ON UPDATE NO ACTION ON DELETE CASCADE )")
+        database.execSQL("INSERT INTO ContatoCopia SELECT * FROM Contato")
+        database.execSQL("DROP TABLE Contato")
+        database.execSQL("ALTER TABLE ContatoCopia RENAME TO Contato")
+    }
 }
 
 @Module
@@ -34,7 +42,7 @@ class DatabaseModule {
             context,
             HelloAppDatabase::class.java,
             DATABASE_NAME
-        ).addMigrations(MIGRATION_1_2)
+        ).addMigrations(MIGRATION_1_2, MIGRATION_5_6)
             .build()
     }
 
